abf8469ed8370158b868d3a1c5e765dab438ac1d
agatov <agatovs@gmail.com>
2014-04-30 19:02:23 +0400

    fix

--- app/assets/javascripts/application/order.js.coffee
+++ app/assets/javascripts/application/order.js.coffee
@@ -2,6 +2,37 @@ $ ->
 
 
 
+  $('#send-special-offer').on 'click', ->
+
+    username = $('input[name=username]')
+    phone = $('input[name=phone]')
+
+    shake_fields = []
+
+    if username.val() < 2
+      shake_fields.push username
+
+    if phone.val() < 10
+      shake_fields.push phone
+
+    if shake_fields.length > 0
+      shake_field(field) for field in shake_fields
+      return false
+
+    $.post(
+      '/orders',
+    {
+      'order[username]': username.val(),
+      'order[email]': phone.val()
+    },
+    (data) =>
+
+    )
+
+    show_thank_you()
+    reachGoal 'new_order'
+
+
   $('#send-sale').on 'click', ->
 
     username = $('input[name=username]')
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,25 @@
 class OrdersController < ApplicationController
 
   def create
-    @order = Order.new params[:order]
 
-    if @order.save
-      render json: {status: :ok}
-    else
-      render json: {status: :error}
-    end
+    message = "#{params[:order][:email]} - #{params[:order][:phone]}"
+
+    Pony.mail ({
+        to: 'admass100@gmail.com, abardacha@gmail.com',
+        subject: I18n.t('email.special_offer_title'),
+        body: message,
+        via: :smtp,
+        via_options: {
+        address: 'smtp.gmail.com',
+        port: 587,
+        enable_starttls_auto: true,
+        user_name: 'admass100',
+        password: 'lakys100',
+        authentication: :plain
+      }
+    })
+
+    render json: {status: :ok}
   end
 
 end
